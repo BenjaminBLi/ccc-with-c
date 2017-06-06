a1e29dec6300261357572fed77060125767dc87f
BenjaminBLi <li.benjamin.b@gmail.com>
2017-06-06 18:45:38 -0400

    Finished Dreaming (ioi13p1.cpp)

--- DMOJ/ioi13p1.cpp
+++ DMOJ/ioi13p1.cpp
@@ -3,22 +3,20 @@ using namespace std;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
 
-int ansV, D, p[100010], w[100010];
+int ansV, D, p[100010], w[100010], id;
 vii adj[100010];
 bool totV[100010];
 
 void dfs(int u, int pre, int cSz){
-    //printf("%d %d: %d\n", u, pre, cSz);
     p[u] = pre;
     totV[u] = true;
     if(D <= cSz){
         ansV = u;
         D = cSz;
-        //cout << "YES: " << D << ", " << ansV << endl;
     }
-    for(ii v : adj[u]){
+    for(ii &v : adj[u]){
         if(v.first == pre) continue;
-        if(!totV[v.first]) w[v.first] = v.second, dfs(v.first, u, cSz+v.second);
+        if(!id == !totV[v.first]) w[v.first] = v.second, dfs(v.first, u, cSz + v.second);
     }
 }
 
@@ -31,29 +29,29 @@ int travelTime(int N, int M, int L, int A[], int B[], int T[]){
 
     int r1 = 0, r2 = 0, r3 = 0, d, R, maxD = 0;
     memset(p, -1, sizeof(p));
+    int rCnt = 0;
     for(int i = 0; i < N; i++){
         if(p[i] != -1) continue;
         ansV = i, D = 0;
-        memset(totV, false, sizeof(totV));
-        dfs(i, -2, 0);
-        memset(totV, false, sizeof(totV));
-        //cout << ansV << ", ";
+        id = 0;
+        dfs(ansV, -2, 0);
+        id = 1;
         dfs(ansV, -2, 0);
         maxD = max(maxD, D);
-        //cout << ansV << endl;
         R = D, d = 0;
         for(int u = ansV; p[u] != -2; u = p[u]){
-           // cout << u  << endl;
             R = min(R, max(D-d, d));
             d += w[u];
         }
-        //cout << endl;
-        if(r1 < R) r2 = r1, r1 = R;
+        rCnt++;
+        if(r1 < R) r3 = r2, r2 = r1, r1 = R;
         else if(r2 < R) r3 = r2, r2 = R;
         else if(r3 < R) r3 = R;
     }
-    //printf("%d %d %d\n", r1, r2, r3);
-    return max(maxD, max(r1+r2+L, r2+r3+2*L));
+    int ans = maxD;
+    if(rCnt >= 2) ans = max(ans, r1+r2+L);
+    if(rCnt >= 3) ans = max(ans, r2+r3+2*L);
+    return ans;
 }
 
 int main(){
