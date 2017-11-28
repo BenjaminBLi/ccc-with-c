e9d66c5dc3a1fabd657317a3f9ac82d2047db906
BenjaminBLi <li.benjamin.b@gmail.com>
2017-11-27 20:01:50 -0500

    completed Jumping and APIO Commando

--- DMOJAndWCIPEG/apio10p1.cpp
+++ DMOJAndWCIPEG/apio10p1.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
-#define fori(i, st, en) for(int i = st; i < en; i++)
-#define rfori(i, st, en) for(int i = st; i >= en; i--)
+#define fori(i, st, en) for(int i = st; i < (int) en; i++)
+#define rfori(i, st, en) for(int i = st; i >= (int) en; i--)
 #define f first
 #define s second
 #define pb push_back
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
 using namespace std;
-typedef unsigned long long ull;
+typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef long long ll;
 typedef vector<ii> vii;
 
-int n, a, b, c, arr[1000010];
-int memo[1000010];
+int n;
+ll a, b, c, val[1000010];
+ll memo[1000010];
 
-inline int val(int x){
-    return x*x*a + x*b + c;
+ll f(int l, int r){
+	int sm = val[r] - val[l-1];
+	return a * sm*sm + b * sm + c; 
 }
 
 int main(){
-    scanf("%d %d %d %d", &n, &a, &b, &c);
-    fori(i, 1, n+1) {
-        scanf("%d", arr+i);
-        arr[i] += arr[i-1];
-    }
-    rfori(i, n, 0) rfori(j, i-1, 0) memo[i] = max(memo[i], memo[j] + val(arr[i]-arr[j]));
-    fori(i, 0, n+1) cout << memo[i] << ", "; cout << endl;
-    cout << memo[1] << endl;
-
-    return 0;
+	scanf("%d%lld%lld%lld", &n, &a, &b, &c);
+	fori(i, 1, n+1) scanf("%lld", val+i), val[i] += val[i-1];
+	memo[0] = 0;
+	int st = 1;
+	fori(i, 1, n+1){
+		memo[i] = f(1, i);
+		fori(j, st, i){
+			if(memo[j] + f(j + 1, i) >= memo[i]){
+				st = j;
+				memo[i] = memo[j] + f(j + 1, i);
+			}
+		}
+	}
+	printf("%lld\n", memo[n]);
+	return 0;
 }
+
