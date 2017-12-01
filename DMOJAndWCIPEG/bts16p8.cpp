7a3056a7b7a732ffcb2e896c829ed29fa732f17d
Ben <13855878+BenjaminBLi@users.noreply.github.com>
2017-11-30 20:38:24 -0500

    Update bts16p8.cpp

--- DMOJAndWCIPEG/bts16p8.cpp
+++ DMOJAndWCIPEG/bts16p8.cpp
@@ -16,26 +16,29 @@ typedef pair<ull, int> ui;
 typedef long long ll;
 
 
-const int MAXN = 200000;
+const int MAXN = 500000, mod = 1e9 + 7;
 
 struct node{
 	ll lazy, sum, mx;
 	int l, r;
 	bool flag;
 	node(){lazy = sum = mx = 0, flag = false;}
-} tree[MAXN<<2];
+} tree[MAXN*6];
 
-int m, q, n, cnt;
+int n, cnt;
 ll orig[MAXN];
+unordered_map<ll, int> compx;
+ll rev[MAXN<<1];
 
 inline void calc(int idx){
 	tree[idx].sum = tree[left(idx)].sum + tree[right(idx)].sum;
+	tree[idx].mx = max(tree[left(idx)].mx, tree[right(idx)].mx);
 }
 
 inline void apply(int idx, ll lazy){
-	tree[idx].lazy += lazy;
+	tree[idx].lazy = tree[idx].mx = lazy;
 	tree[idx].flag = true;
-	tree[idx].sum += lazy * (tree[idx].r - tree[idx].l+1);
+	tree[idx].sum = lazy * (rev[tree[idx].r] - rev[tree[idx].l]);
 }
 
 inline void prop(int idx){
@@ -43,16 +46,12 @@ inline void prop(int idx){
 	apply(left(idx), tree[idx].lazy);
 	apply(right(idx), tree[idx].lazy);
 	tree[idx].flag = false;
-	tree[idx].lazy = 0;
 }
 
 void build(int l, int r, int idx = 1){
 	tree[idx].l = l, tree[idx].r = r;
-	if(l == r) {
-		tree[idx].sum = orig[l];
-		return;
-	}
-	build(l, mid(l, r), left(idx)), build(mid(l, r) + 1, r, right(idx));
+	if(l == r-1) return;
+	build(l, mid(l, r), left(idx)), build(mid(l, r) , r, right(idx));
 	calc(idx);
 }
 
@@ -61,7 +60,7 @@ void upd(int l, int r, ll val, int idx = 1){
 		apply(idx, val);
 		return;
 	}
-	if(tree[idx].r < l || tree[idx].l > r) return;
+	if(tree[idx].r <= l || tree[idx].l >= r) return;
 	prop(idx);
 	upd(l, r, val, left(idx)), upd(l, r, val, right(idx));
 	calc(idx);
@@ -70,28 +69,52 @@ void upd(int l, int r, ll val, int idx = 1){
 ll qrySum(int l, int r, int idx = 1){
 	if(tree[idx].l >= l && tree[idx].r <= r)
 		return tree[idx].sum;
-	if(tree[idx].r < l || tree[idx].l > r) return 0;
+	if(tree[idx].r <= l || tree[idx].l >= r) return 0;
 	prop(idx);
 	return qrySum(l, r, left(idx)) + qrySum(l, r, right(idx)); 
 }
 
-int main(){
-	scanf("%d%d%d", &m, &n, &q);
-	fori(i, 0, n) scanf("%lld", orig+i);
-	build(0, n-1);
-	int id, l, r;
-	ll x;
+ll qryMx(int l, int r, int idx = 1){
+	if(tree[idx].l >= l && tree[idx].r <= r)
+		return tree[idx].mx;
+	if(tree[idx].r <= l || tree[idx].l >= r) return 0;
+	prop(idx);
+	return max(qryMx(l, r, left(idx)), qryMx(l, r, right(idx)));
+}
 
-	for(;q--;){
-		scanf("%d%d%d", &id, &l, &r);
-		l--, r--;
-		if(id == 1){
-			scanf("%lld", &x);
-			upd(l, r, x);
-		}else{
-			printf("%lld\n", qrySum(l, r) % m);
-		}
+
+struct book{
+	int id;
+	ll s, l, w;
+	book(){s = l = w = 0;}
+}books[MAXN];
+
+int main(){
+	//I wonder if I mle....
+	scanf("%d", &n);
+	fori(i, 0, n){
+		books[i].id = i;
+		scanf("%lld%lld%lld", &books[i].s, &books[i].l, &books[i].w);
+		rev[left(i)] = books[i].s;
+		rev[right(i)] = books[i].s + books[i].l;
 	}
+	sort(rev, rev + (n<<1));
+	int cnt = unique(rev, rev + (n<<1)) - rev;
+	fori(i, 0, cnt) compx[rev[i]] = i;
+
+	ll ans = 0;
+	build(0, cnt);
+
+	fori(i, 0, n){
+		int l = compx[books[i].s], r = compx[books[i].s+books[i].l];
+		ll currH = qryMx(l, r);
+		ll covered = qrySum(l, r);
+		ans += 1LL * (books[i].l) * currH - covered;
+		upd(l, r, currH + books[i].w);
+	}
+
+	cout << ans%mod << endl;
+	
 
 	return 0;
 }
